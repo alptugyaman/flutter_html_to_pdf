f6900296e6a22643dd4d828fcbc0505f11e45ca6
Alptuğ Yaman <alptugyaman@hotmail.com>
2024-04-17 09:06:01 +0300

    Update SwiftFlutterHtmlToPdfPlugin.swift

--- ios/Classes/SwiftFlutterHtmlToPdfPlugin.swift
+++ ios/Classes/SwiftFlutterHtmlToPdfPlugin.swift
@@ -2,58 +2,72 @@ import Flutter
 import UIKit
 import WebKit
 
-public class SwiftFlutterHtmlToPdfPlugin: NSObject, FlutterPlugin{
-    var wkWebView : WKWebView!
+public class SwiftFlutterHtmlToPdfPlugin: NSObject, FlutterPlugin {
+    var wkWebView: WKWebView!
     var urlObservation: NSKeyValueObservation?
-    
+
     public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_html_to_pdf", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterHtmlToPdfPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-    
+        let channel = FlutterMethodChannel(name: "flutter_html_to_pdf", binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterHtmlToPdfPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "convertHtmlToPdf":
-        let args = call.arguments as? [String: Any]
-        let htmlFilePath = args!["htmlFilePath"] as? String
-        
-        // !!! this is workaround for issue with rendering PDF images on iOS !!!
-        let viewControler = UIApplication.shared.delegate?.window?!.rootViewController
-        wkWebView = WKWebView.init(frame: viewControler!.view.bounds)
-        wkWebView.isHidden = true
-        wkWebView.tag = 100
-        viewControler?.view.addSubview(wkWebView)
-        
-        let htmlFileContent = FileHelper.getContent(from: htmlFilePath!) // get html content from file
-        wkWebView.loadHTMLString(htmlFileContent, baseURL: Bundle.main.bundleURL) // load html into hidden webview
-        
-        urlObservation = wkWebView.observe(\.isLoading, changeHandler: { (webView, change) in
-            // this is workaround for issue with loading local images
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                let convertedFileURL = PDFCreator.create(printFormatter: self.wkWebView.viewPrintFormatter())
-                let convertedFilePath = convertedFileURL.absoluteString.replacingOccurrences(of: "file://", with: "") // return generated pdf path
-                if let viewWithTag = viewControler?.view.viewWithTag(100) {
-                    viewWithTag.removeFromSuperview() // remove hidden webview when pdf is generated
-                    
-                    // clear WKWebView cache
-                    if #available(iOS 9.0, *) {
-                        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
-                            records.forEach { record in
-                                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+        switch call.method {
+        case "convertHtmlToPdf":
+            guard let args = call.arguments as? [String: Any] else {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "Invalid argument for htmlFilePath", details: nil))
+                return
+            }
+            
+            guard let htmlFilePath = args["htmlFilePath"] as? String else {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "htmlFilePath argument is missing or invalid", details: nil))
+                return
+            }
+
+            let viewController: UIViewController
+            if let window = UIApplication.shared.delegate?.window {
+                viewController = window?.rootViewController ?? UIViewController()
+            } else {
+                viewController = UIViewController()
+            }
+
+            wkWebView = WKWebView(frame: viewController.view.bounds)
+            wkWebView.isHidden = true
+            wkWebView.tag = 100
+            viewController.view.addSubview(wkWebView)
+
+            if let htmlFileContent = FileHelper.getContent(from: htmlFilePath) as? String {
+                wkWebView.loadHTMLString(htmlFileContent, baseURL: Bundle.main.bundleURL)
+
+                urlObservation = wkWebView.observe(\.isLoading, changeHandler: { [weak self] (webView, change) in
+                    guard let self = self else { return }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                        let convertedFileURL = PDFCreator.create(printFormatter: self.wkWebView.viewPrintFormatter())
+                        let convertedFilePath = convertedFileURL.absoluteString.replacingOccurrences(of: "file://", with: "")
+
+                        if let viewWithTag = viewController.view.viewWithTag(100) {
+                            viewWithTag.removeFromSuperview()
+
+                            if #available(iOS 9.0, *) {
+                                WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+                                    records.forEach { record in
+                                        WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+                                    }
+                                }
                             }
                         }
+
+                        self.urlObservation = nil
+                        self.wkWebView = nil
+                        result(convertedFilePath)
                     }
-                }
-                
-                // dispose WKWebView
-                self.urlObservation = nil
-                self.wkWebView = nil
-                result(convertedFilePath)
+                })
+            } else {
+                result(FlutterError(code: "HTML_CONTENT_ERROR", message: "HTML file content could not be retrieved or converted to String.", details: nil))
             }
-        })
-    default:
-        result(FlutterMethodNotImplemented)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
     }
-  }
 }
